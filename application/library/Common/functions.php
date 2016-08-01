5824f26d89bb61f16924d0128b6a5999eda07166
jsyzchen <jsyzchenchen@gmail.com>
2016-08-01 09:27:57 +0800

    函数库

---
+++ application/library/Common/functions.php
@@ -0,0 +1,404 @@
+<?php
+/***
+ * 函数库
+ * @author jsyzchenchen@gmail.com
+ * date 2015-11-30
+ */
+
+
+/**
+ * PHP生成指定随机字符串的简单实现方法
+ * @param $length
+ * @param string $type
+ * @return string
+ */
+function get_nonce_str($length,$type="number,upper,lower"){
+    $valid_type = array('number','upper','lower');
+    $case = explode(",",$type);
+    $count = count($case);
+    //根据交集判断参数是否合法
+    if($count !== count(array_intersect($case,$valid_type))){
+        return false;
+    }
+    $lower = "abcdefghijklmnopqrstuvwxyz";
+    $upper = strtoupper($lower);
+    $number = "0123456789";
+    $str_list = "";
+    for($i=0;$i<$count;++$i){
+        $str_list .= $$case[$i];
+    }
+    return substr(str_shuffle($str_list),0,$length);
+}
+
+/**
+ * 获取输入参数 支持过滤和默认值 From ThinkPHP 系统函数库(I函数)
+ * 使用方法:
+ * <code>
+ * input('id',0); 获取id参数 自动判断get或者post
+ * input('post.name','','htmlspecialchars'); 获取$_POST['name']
+ * input('get.'); 获取$_GET
+ * </code>
+ * @param string $name 变量的名称 支持指定类型
+ * @param mixed $default 不存在的时候默认值
+ * @param mixed $filter 参数过滤方法
+ * @param mixed $datas 要获取的额外数据源
+ * @return mixed
+ */
+function input($name,$default='',$filter=null,$datas=null) {
+    static $_PUT	=	null;
+    if(strpos($name,'/')){ // 指定修饰符
+        list($name,$type) 	=	explode('/',$name,2);
+    }else{ // 默认强制转换为字符串
+        $type   =   's';
+    }
+    if(strpos($name,'.')) { // 指定参数来源
+        list($method,$name) =   explode('.',$name,2);
+    }else{ // 默认为自动判断
+        $method =   'param';
+    }
+    switch(strtolower($method)) {
+        case 'get'     :
+            $input =& $_GET;
+            break;
+        case 'post'    :
+            $input =& $_POST;
+            break;
+        case 'put'     :
+            if(is_null($_PUT)){
+                parse_str(file_get_contents('php://input'), $_PUT);
+            }
+            $input 	=	$_PUT;
+            break;
+        case 'param'   :
+            switch($_SERVER['REQUEST_METHOD']) {
+                case 'POST':
+                    $input  =  $_POST;
+                    break;
+                case 'PUT':
+                    if(is_null($_PUT)){
+                        parse_str(file_get_contents('php://input'), $_PUT);
+                    }
+                    $input 	=	$_PUT;
+                    break;
+                default:
+                    $input  =  $_GET;
+            }
+            break;
+        case 'path'    :
+            $input  =   array();
+            if(!empty($_SERVER['PATH_INFO'])){
+                $depr   =   '/';
+                $input  =   explode($depr,trim($_SERVER['PATH_INFO'],$depr));
+            }
+            break;
+        case 'request' :
+            $input =& $_REQUEST;
+            break;
+        case 'session' :
+            $input =& $_SESSION;
+            break;
+        case 'cookie'  :
+            $input =& $_COOKIE;
+            break;
+        case 'server'  :
+            $input =& $_SERVER;
+            break;
+        case 'globals' :
+            $input =& $GLOBALS;
+            break;
+        case 'data'    :
+            $input =& $datas;
+            break;
+        default:
+            return null;
+    }
+    if(''==$name) { // 获取全部变量
+        $data       =   $input;
+        $filters    =   isset($filter)?$filter:get_config('user','default_filter');
+        if($filters) {
+            if(is_string($filters)){
+                $filters    =   explode(',',$filters);
+            }
+            foreach($filters as $filter){
+                $data   =   array_map_recursive($filter,$data); // 参数过滤
+            }
+        }
+    }elseif(isset($input[$name])) { // 取值操作
+        $data       =   $input[$name];
+        $filters    =   isset($filter)?$filter:get_config('user','default_filter');
+        if($filters) {
+            if(is_string($filters)){
+                if(0 === strpos($filters,'/')){
+                    if(1 !== preg_match($filters,(string)$data)){
+                        // 支持正则验证
+                        return   isset($default) ? $default : null;
+                    }
+                }else{
+                    $filters    =   explode(',',$filters);
+                }
+            }elseif(is_int($filters)){
+                $filters    =   array($filters);
+            }
+
+            if(is_array($filters)){
+                foreach($filters as $filter){
+                    if(function_exists($filter)) {
+                        $data   =   is_array($data) ? array_map_recursive($filter,$data) : $filter($data); // 参数过滤
+                    }else{
+                        $data   =   filter_var($data,is_int($filter) ? $filter : filter_id($filter));
+                        if(false === $data) {
+                            return   isset($default) ? $default : null;
+                        }
+                    }
+                }
+            }
+        }
+        if(!empty($type)){
+            switch(strtolower($type)){
+                case 'a':	// 数组
+                    $data 	=	(array)$data;
+                    break;
+                case 'd':	// 数字
+                    $data 	=	(int)$data;
+                    break;
+                case 'f':	// 浮点
+                    $data 	=	(float)$data;
+                    break;
+                case 'b':	// 布尔
+                    $data 	=	(boolean)$data;
+                    break;
+                case 's':   // 字符串
+                default:
+                    $data   =   (string)$data;
+            }
+        }
+    }else{ // 变量默认值
+        $data       =    isset($default)?$default:null;
+    }
+    is_array($data) && array_walk_recursive($data,'other_safe_filter');
+    return $data;
+}
+
+/**
+ * 其他安全过滤 From ThinkPHP 系统函数库 为input函数服务
+ * @param $value
+ */
+function other_safe_filter(&$value)
+{
+    // TODO 其他安全过滤
+    // 过滤查询特殊字符
+    if (preg_match('/^(EXP|NEQ|GT|EGT|LT|ELT|OR|XOR|LIKE|NOTLIKE|NOT BETWEEN|NOTBETWEEN|BETWEEN|NOTIN|NOT IN|IN)$/i', $value)) {
+        $value .= ' ';
+    }
+}
+
+/**
+ * 获取用户定义的局部config for yaf
+ * @param string $name
+ * @return mixed
+ */
+function get_config($prefix=null, $name=null){
+    static $configs=null;
+    if(!is_array($configs)){
+        $configs = $GLOBALS["config"];
+    }
+    if(is_null($prefix)){
+        return $configs;
+    }else{
+        if(is_null($name)){
+            if(isset($configs[$prefix])){
+                return $configs[$prefix];
+            }else{
+                return null;
+            }
+        }else{
+            if(isset($configs[$prefix][$name])){
+                return $configs[$prefix];
+            }else{
+                return null;
+            }
+        }
+    }
+}
+
+/**
+ * 获取客户端IP地址 FROM ThinkPHP 系统函数库
+ * @param integer $type 返回类型 0 返回IP地址 1 返回IPV4地址数字
+ * @param boolean $adv 是否进行高级模式获取（有可能被伪装）
+ * @return mixed
+ */
+function get_client_ip($type = 0,$adv=false) {
+    $type       =  $type ? 1 : 0;
+    static $ip  =   NULL;
+    if ($ip !== NULL) return $ip[$type];
+    if($adv){
+        if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $arr    =   explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+            $pos    =   array_search('unknown',$arr);
+            if(false !== $pos) unset($arr[$pos]);
+            $ip     =   trim($arr[0]);
+        }elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip     =   $_SERVER['HTTP_CLIENT_IP'];
+        }elseif (isset($_SERVER['REMOTE_ADDR'])) {
+            $ip     =   $_SERVER['REMOTE_ADDR'];
+        }
+    }elseif (isset($_SERVER['REMOTE_ADDR'])) {
+        $ip     =   $_SERVER['REMOTE_ADDR'];
+    }
+    // IP地址合法验证
+    $long = sprintf("%u",ip2long($ip));
+    $ip   = $long ? array($ip, $long) : array('0.0.0.0', 0);
+    return $ip[$type];
+}
+
+/**
+ * URL重定向 FROM ThinkPHP 系统函数库
+ * @param string $url 重定向的URL地址
+ * @param integer $time 重定向的等待时间（秒）
+ * @param string $msg 重定向前的提示信息
+ * @return void
+ */
+function redirect($url, $time=0, $msg='') {
+    //多行URL地址支持
+    $url        = str_replace(array("\n", "\r"), '', $url);
+    if (empty($msg))
+        $msg    = "系统将在{$time}秒之后自动跳转到{$url}！";
+    if (!headers_sent()) {
+        // redirect
+        if (0 === $time) {
+            header('Location: ' . $url);
+        } else {
+            header("refresh:{$time};url={$url}");
+            echo($msg);
+        }
+        exit();
+    } else {
+        $str    = "<meta http-equiv='Refresh' content='{$time};URL={$url}'>";
+        if ($time != 0)
+            $str .= $msg;
+        exit($str);
+    }
+}
+
+
+/**
+ * GET 请求 FROM wechat-php-sdk
+ * @param string $url
+ */
+function http_get($url){
+    $oCurl = curl_init();
+    if(stripos($url,"https://")!==FALSE){
+        curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, FALSE);
+        curl_setopt($oCurl, CURLOPT_SSLVERSION, 1); //CURL_SSLVERSION_TLSv1
+    }
+    curl_setopt($oCurl, CURLOPT_URL, $url);
+    curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1 );
+    $sContent = curl_exec($oCurl);
+    $aStatus = curl_getinfo($oCurl);
+    curl_close($oCurl);
+    if(intval($aStatus["http_code"])==200){
+        return $sContent;
+    }else{
+        return false;
+    }
+}
+
+/**
+ * POST 请求 FROM wechat-php-sdk
+ * @param string $url
+ * @param array $param
+ * @param boolean $post_file 是否文件上传
+ * @return string content
+ */
+function http_post($url,$param,$post_file=false){
+    $oCurl = curl_init();
+    if(stripos($url,"https://")!==FALSE){
+        curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($oCurl, CURLOPT_SSLVERSION, 1); //CURL_SSLVERSION_TLSv1
+    }
+    if (is_string($param) || $post_file) {
+        $strPOST = $param;
+    } else {
+        $aPOST = array();
+        foreach($param as $key=>$val){
+            $aPOST[] = $key."=".urlencode($val);
+        }
+        $strPOST =  join("&", $aPOST);
+    }
+    curl_setopt($oCurl, CURLOPT_URL, $url);
+    curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1 );
+    curl_setopt($oCurl, CURLOPT_POST,true);
+    curl_setopt($oCurl, CURLOPT_POSTFIELDS,$strPOST);
+    $sContent = curl_exec($oCurl);
+    $aStatus = curl_getinfo($oCurl);
+    curl_close($oCurl);
+    if(intval($aStatus["http_code"])==200){
+        return $sContent;
+    }else{
+        return false;
+    }
+}
+
+
+/**
+ * curl 批处理
+ * @param $url_array
+ * @return array
+ * @author jsyzchenchen@gmail.com
+ */
+function curl_multi($url_array,$curlopt_timeout=10){
+    $handles = $contents = array();
+
+    //初始化curl multi对象
+    $mh = curl_multi_init();
+
+    //添加curl 批处理会话
+    foreach($url_array as $key => $url)
+    {
+        $handles[$key] = curl_init($url);
+        curl_setopt($handles[$key], CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($handles[$key], CURLOPT_TIMEOUT, $curlopt_timeout);
+
+        curl_multi_add_handle($mh, $handles[$key]);
+    }
+
+    //======================执行批处理句柄=================================
+    $active = null;
+    do {
+        $mrc = curl_multi_exec($mh, $active);
+    } while ($mrc == CURLM_CALL_MULTI_PERFORM);
+
+
+    while ($active and $mrc == CURLM_OK) {
+
+        if(curl_multi_select($mh) === -1){
+            usleep(100);
+        }
+        do {
+            $mrc = curl_multi_exec($mh, $active);
+        } while ($mrc == CURLM_CALL_MULTI_PERFORM);
+
+    }
+    //====================================================================
+
+    //获取批处理内容
+    foreach($handles as $i => $ch)
+    {
+        $content = curl_multi_getcontent($ch);
+        $contents[$i] = curl_errno($ch) == 0 ? $content : '';
+    }
+
+    //移除批处理句柄
+    foreach($handles as $ch)
+    {
+        curl_multi_remove_handle($mh, $ch);
+    }
+
+    //关闭批处理句柄
+    curl_multi_close($mh);
+
+    return $contents;
+}
+?>
