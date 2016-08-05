7ee7784d0eeaf754b335133d8b978d13321d8ead
jsyzchen <jsyzchenchen@gmail.com>
2016-08-05 16:49:12 +0800

    update

--- public/index.php
+++ public/index.php
@@ -1,8 +1,16 @@
 <?php
+header('content-type:text/html;charset=utf-8');
+
 define("APP_PATH",  realpath(dirname(__FILE__) . '/../')); /* 指向public的上一级 */
 
-// 加载 Composer
-require APP_PATH . '/vendor/autoload.php';
+// 设置为“开发调试”模式
+define ( "APP_DEBUG", true ); // 调试
+// define("APP_DEBUG",false);//生产
+if(APP_DEBUG){
+    error_reporting(E_ALL);//使用error_reporting来定义哪些级别错误可以触发
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+}
 
 $app  = new Yaf\Application(APP_PATH . "/conf/application.ini");
 $app->bootstrap()->run();
