dbb76f5d293e754aac030760c903bf3cf8267979
jsyzchen <jsyzchenchen@gmail.com>
2016-08-05 16:48:58 +0800

    update

--- public/yarserver.php
+++ public/yarserver.php
@@ -1,10 +1,18 @@
 <?php
-define('APPLICATION_PATH', dirname(__FILE__) . "/../");
+header('content-type:text/html;charset=utf-8');
 
-// 加载 Composer
-require APPLICATION_PATH . '/vendor/autoload.php';
+define("APP_PATH",  realpath(dirname(__FILE__) . '/../')); /* 指向public的上一级 */
 
-$application = new Yaf\Application( APPLICATION_PATH . "/conf/yarserver.ini");
+// 设置为“开发调试”模式
+define ( "APP_DEBUG", true ); // 调试
+// define("APP_DEBUG",false);//生产
+if(APP_DEBUG){
+    error_reporting(E_ALL);//使用error_reporting来定义哪些级别错误可以触发
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+}
+
+$application = new Yaf\Application( APP_PATH . "/conf/yarserver.ini");
 
 $application->bootstrap()->run();
 ?>
