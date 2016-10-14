d610d0555b208e9b78bd0c79f0d788c8f7fbc84c
jsyzchen <jsyzchenchen@gmail.com>
2016-10-14 23:50:47 +0800

    update

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -7,23 +7,15 @@
  * 这些方法, 都接受一个参数:Yaf_Dispatcher $dispatcher
  * 调用的次序, 和申明的次序相同
  */
-use Yaf\Loader;
-use Yaf\Registry;
-use Yaf\Dispatcher;
-use Yaf\Application;
-use Yaf\Bootstrap_Abstract;
-
-class Bootstrap extends Bootstrap_Abstract{
-
-	private $_config;
+class Bootstrap extends \Yaf\Bootstrap_Abstract{
+    /** @var object config */
+	private $config;
 
     /**
      * 加载
      */
     public function _initLoader(){
-        if(file_exists(APP_PATH . '/vendor/autoload.php')){
-            Loader::import(APP_PATH . '/vendor/autoload.php');
-        }
+        \Yaf\Loader::import(APP_PATH . '/vendor/autoload.php');
     }
 
     /**
@@ -31,19 +23,19 @@ class Bootstrap extends Bootstrap_Abstract{
      */
     public function _initConfig() {
 		//把配置保存起来
-		$this->_config = Application::app()->getConfig();
-		Registry::set('config', $this->_config);
+		$this->config = \Yaf\Application::app()->getConfig();
+        \Yaf\Registry::set('config', $this->config);
 	}
 
     /**
      * 日志
-     * @param Dispatcher $dispatcher
+     * @param \Yaf\Dispatcher $dispatcher
      */
-	public function _initLogger(Dispatcher $dispatcher){
+	public function _initLogger(\Yaf\Dispatcher $dispatcher){
         //SocketLog
-        if($this->_config->socketlog->enable){
+        if($this->config->socketlog->enable){
             //载入
-            Loader::import('Common/Logger/slog.function.php');
+            \Yaf\Loader::import('Common/Logger/slog.function.php');
             //配置SocketLog
             slog($this->_config->socketlog->toArray(),'config');
         }
@@ -51,9 +43,9 @@ class Bootstrap extends Bootstrap_Abstract{
 
     /**
      * 插件
-     * @param Dispatcher $dispatcher
+     * @param \Yaf\Dispatcher $dispatcher
      */
-	public function _initPlugin(Dispatcher $dispatcher) {
+	public function _initPlugin(\Yaf\Dispatcher $dispatcher) {
 		//注册一个插件
 		//$objSamplePlugin = new SamplePlugin();
 		//$dispatcher->registerPlugin($objSamplePlugin);
@@ -61,9 +53,9 @@ class Bootstrap extends Bootstrap_Abstract{
 
     /**
      * 路由
-     * @param Dispatcher $dispatcher
+     * @param \Yaf\Dispatcher $dispatcher
      */
-	public function _initRoute(Dispatcher $dispatcher) {
+	public function _initRoute(\Yaf\Dispatcher $dispatcher) {
 		//在这里注册自己的路由协议,默认使用简单路由
 	}
 
@@ -71,39 +63,19 @@ class Bootstrap extends Bootstrap_Abstract{
      * LocalName
      */
 	public function _initLocalName() {
-		Loader::getInstance()->registerLocalNamespace(array(
-			'Common','Smarty'
-		));
+
 	}
 
-    /**
-     * 视图
-     * @param Dispatcher $dispatcher
-     */
-	public function _initView(Dispatcher $dispatcher) {
-		//在这里注册自己的view控制器，例如smarty,firekylin
-		/* init smarty view engine */
-		if('Smarty' == $this->_config->application->view->name){
-			Loader::import("Smarty/Adapter.php");		
-			$smarty = new Adapter(null, Registry::get("config")->get("smarty"));
-			$dispatcher->setView($smarty);
-		}		
-	}
 
     /**
-     * Db,若使用Facedes,则无需使用该方法
+     * Twig View
+     * @param \Yaf\Dispatcher $dispatcher
      */
-	/*public function _initDefaultDbAdapter()
-	{  
-        $capsule = new \Illuminate\Database\Capsule\Manager;
-        $capsule->addConnection($this->_config->database->toArray());
-        $capsule->setEventDispatcher(new \Illuminate\Events\Dispatcher(new \Illuminate\Container\Container));
-        $capsule->setAsGlobal();
-
-        //开启Eloquent ORM
-        $capsule->bootEloquent();
-  
-	}*/
+    protected function _initTwig(\Yaf\Dispatcher $dispatcher)
+    {
+        $twig = new \Twig\Adapter(APP_PATH . "/application/views/", $this->config->get("twig")->toArray());
+        $dispatcher->setView($twig);
+    }
 
 	/**
      * 初始化facades
@@ -118,7 +90,7 @@ class Bootstrap extends Bootstrap_Abstract{
             'fetch' => \PDO::FETCH_CLASS,
             'default' => 'mysql',
             'connections' => [
-                'mysql' => $this->_config->database->toArray()
+                'mysql' => $this->config->database->toArray()
             ],
         ];
         $config->set('database', $database_config);
@@ -127,12 +99,12 @@ class Bootstrap extends Bootstrap_Abstract{
         \Illuminate\Support\Facades\Facade::clearResolvedInstances();
         \Illuminate\Support\Facades\Facade::setFacadeApplication($container);
         $aliases = array();
-        Loader::import(APPLICATION_PATH . '/conf/aliases.php');
+        \Yaf\Loader::import(APP_PATH . '/conf/aliases.php');
         \Common\AliasLoader::getInstance($aliases)->register();
 
         //RegisterProviders
         $providers = array();
-        Loader::import(APPLICATION_PATH . '/conf/providers.php');
+        \Yaf\Loader::import(APP_PATH . '/conf/providers.php');
         foreach ($providers as $provider) {
             $provider = new $provider($container);
             $provider->register();
@@ -146,6 +118,6 @@ class Bootstrap extends Bootstrap_Abstract{
      * 公用函数
      */
     public function _initFunction(){
-        Loader::import('Common/functions.php');
+        \Yaf\Loader::import('Common/functions.php');
     }
 }
