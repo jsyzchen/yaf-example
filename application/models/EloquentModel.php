de5c0e0f70339c95335a571e8f8cfc4ca3ee84cd
jsyzchen <jsyzchenchen@gmail.com>
2016-10-14 23:57:34 +0800

    update

--- application/models/EloquentModel.php
+++ application/models/EloquentModel.php
@@ -1,16 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: chenchen
- * Date: 15/11/27
- * Time: 17:18
- */
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class EloquentModel extends Model
 {
-    // todo
+
 }
